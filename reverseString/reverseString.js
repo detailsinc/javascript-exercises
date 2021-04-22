573240ec3046c5b60894f578d71ac90c8d213781
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-04-22 04:44:45 -0700

    Solved
    Take and reverse string. Personal Note: Practice more

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
-}
+const reverseString = function(string) {
+    let newString = string.split('');
+    let stringText = ''
+    for (let i = newString.length; i > 0; i--) {
+        stringText += newString[i - 1];
+    }
+    return stringText;
+} 
 
 module.exports = reverseString
