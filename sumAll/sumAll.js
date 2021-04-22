ba6b8ce4768c5a058dd4cb18c6f7eee8d4e09202
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-04-22 04:43:45 -0700

    Solved
    loop and add all i values given by function parameters.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = 0;
+    let numHolder
+    if (!(typeof start === 'number') || !(typeof end === 'number')) {
+        return 'ERROR';
+    }
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    if (start > end) {
+        numHolder = start;
+        start = end;
+        end = numHolder;
+    }
+    for (let i = start; i < end + 1; i++) {
+        result += i
+    } 
+    return result;
 }
 
 module.exports = sumAll
