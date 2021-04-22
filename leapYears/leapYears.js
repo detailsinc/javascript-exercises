ffc20ac830509632f599b5fd76b0df9f0eb0b2fc
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-04-22 04:46:06 -0700

    Solved
    Added two true conditions and one false

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+        result = true;
+        return result;
+    }
+    if (year % 4 == 0 && !(year % 100 == 0)) {
+        result = true;
+        return result;
+    }
+    else {
+        result = false;
+        return result;
+    }
 }
 
 module.exports = leapYears
