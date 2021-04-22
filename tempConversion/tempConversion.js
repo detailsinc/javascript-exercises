3d019cefe50b658e141487e1faaf8920c5f17296
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-04-22 04:42:30 -0700

    Solved
    set result to respective formula for temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fNum) {
+  let result, rndResult, x;
+  x = fNum;
+  result = (x - 32) * (5 / 9);
+  rndResult = Math.round(result * 10) / 10;
+  return rndResult;
 }
 
-const ctof = function() {
-
+const ctof = function(cNum) {
+  let result, rndResult, x;
+  x = cNum;
+  result = (x * (9 / 5)) + 32;
+  rndResult = Math.round(result * 10) / 10;
+  return rndResult;
 }
 
 module.exports = {
