d8dea5df9f5cdfd0a902ee1c7ef414d24dd55ace
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-04-22 02:02:17 -0700

    Solved
    function repeats input string any number of times and returns 'error' if value is negative.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, repeats) {
+    if (repeats < 0) {
+        return('ERROR');
+    }
+    let string = '';
+    for (let i = 0; i < repeats; i++) {
+        string += phrase;
+    }
+    return string;
 }
 
 module.exports = repeatString
