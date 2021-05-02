3e9d3c69e66de12624e96fc76bbe234f065dcab0
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-05-01 20:44:49 -0700

    Solved

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    let originalArray = string.toLowerCase().replace(/[^A-Za-z]/g, '');
+    let reversedArray = originalArray.split('').reverse().join('');
+    return (originalArray == reversedArray) ? true : false;
+}  
 
-}
 
 module.exports = palindromes
