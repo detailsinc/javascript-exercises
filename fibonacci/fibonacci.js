0bdac8e62a3020cf613022f1baf9c38fca97d2cc
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-05-01 23:42:39 -0700

    Solved

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {return 'OOPS'}
+    let i, lastVal, nextVal, sum;
+    lastVal = 0;
+    nextVal = 1;
+    for (i = 0; i < n - 1; i++) {
+        sum = lastVal + nextVal;
+        lastVal = nextVal;
+        nextVal = sum;
+    }
+    return nextVal;
 
 }
 
 module.exports = fibonacci
+
